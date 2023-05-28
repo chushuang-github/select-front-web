7ee0342b1f5eb169aa9f3d7e806538e5c8791649
chushuang <1214106564@qq.com>
2023-05-28 09:54:16 +0800

    测试husky的pre-commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <h1>App</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+const fun = () =>
+{
+  console.log("还是没有格式化")
+}
+fun()
