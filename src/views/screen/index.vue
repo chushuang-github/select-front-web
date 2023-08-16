98f6d481c7b62275abd9b7f98d9c638e238d34ad
chushuang <1214106564@qq.com>
2023-08-16 23:43:17 +0800

    feat: 大屏顶部完成

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,67 @@
 <template>
-  <div>
-    <h1>数据大屏</h1>
+  <div class="container">
+    <div class="screen" ref="screenRef">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">左侧</div>
+        <div class="center">中间</div>
+        <div class="right">右侧</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Top from './components/top/index.vue'
 
-<style scoped lang="scss"></style>
+const screenRef = ref()
+// 缩放的比例
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+
+onMounted(() => {
+  screenRef.value.style.transform = `scale(${getScale()}) translateX(-50%)`
+})
+
+window.onresize = () => {
+  screenRef.value.style.transform = `scale(${getScale()}) translateX(-50%)`
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url('./images/bg.png') no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    left: 50%;
+    transform-origin: left top;
+    width: 1920px;
+    height: 1080px;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
